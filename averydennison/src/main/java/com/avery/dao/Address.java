c8f4065b6b7207da817a24ce77b7441069f33958
Rakesh kumar <rakesh.kumar@adeptia.com>
2016-11-30 09:54:01 +0530

    update all pojo classes

--- averydennison/src/main/java/com/avery/dao/Address.java
+++ averydennison/src/main/java/com/avery/dao/Address.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-/*author Rakesh*/
+
 
 @Entity
 @Table(name = "address")

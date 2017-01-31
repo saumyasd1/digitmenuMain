5c59cd29cc7e6e22a758f0324e27c735e95e4a0b
Vishal <Vishal@ADLP011_Vishal>
2017-01-31 12:25:19 +0530

    Updated emailutils with logging

--- averydennison/src/main/java/com/avery/utils/EmailUtils.java
+++ averydennison/src/main/java/com/avery/utils/EmailUtils.java
@@ -1,12 +1,29 @@
 package com.avery.utils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
-import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
+import com.adeptia.indigo.logging.Logger;
+import com.avery.EmailManager;
 import com.avery.services.OrderEmailQueueServices;
 
+/**
+ * @author Vishal
+ *
+ */
 public class EmailUtils {
 
 	static {
@@ -30,24 +47,38 @@ public class EmailUtils {
 	 */
 	public static void forwardEML(String fromUserEmailID,
 			String fromUserPassword, String toUserEmailIDList,
-			String emlFilePath, String additionalBodyContent)
+			String emlFilePath, String additionalBodyContent, Logger log)
 			throws MessagingException, IOException {
 		Transport transport = null;
 		InputStream source = null;
 		try {
+			log.debug("additional body content is:\"" + additionalBodyContent
+					+ "\".");
+			log.debug("Creating session at:\"" + EmailManager.getDate() + "\".");
 			Session mailSession = Session.getDefaultInstance(
 					System.getProperties(), null);
+			log.debug("Session has been created at:\"" + EmailManager.getDate()
+					+ "\".");
 			File emlFile = new File(emlFilePath);
 			source = new FileInputStream(emlFile);
 			MimeMessage message = new MimeMessage(mailSession, source);
 			transport = mailSession.getTransport();
+			log.debug("Connecting transport for  user:\"" + fromUserEmailID
+					+ " session at:\"" + EmailManager.getDate() + "\".");
 			transport.connect(fromUserEmailID, fromUserPassword);
+			log.debug("Transport connected successsfully for  user:\""
+					+ fromUserEmailID + " session at:\""
+					+ EmailManager.getDate() + "\".");
 			InternetAddress[] toUserIdArray = InternetAddress
 					.parse(toUserEmailIDList);
 			message.setRecipients(Message.RecipientType.TO, toUserIdArray);
 			addMessageContent(message, additionalBodyContent);
 			message.saveChanges();
+			log.debug("Forwading eml file at:\"" + EmailManager.getDate()
+					+ "\".");
 			transport.sendMessage(message, message.getAllRecipients());
+			log.debug("eml file has been forwarded successfully at:\""
+					+ EmailManager.getDate() + "\".");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -76,7 +107,6 @@ public class EmailUtils {
 				}
 			}
 		}
-		System.out.println("Message sent successfully.");
 	}
 
 	/**
@@ -99,17 +129,32 @@ public class EmailUtils {
 	}
 
 	/**
-	 * @throws Exception
+	 * Method to forward eml file in Email
 	 * 
+	 * @param emailQueueId
+	 * @param fromUserEmailID
+	 * @param fromUserPassword
+	 * @param toUserEmailIDList
+	 * @param additionalBodyContent
+	 * @throws Exception
 	 */
 	public static void forwardEMLFileAsMail(int emailQueueId,
 			String fromUserEmailID, String fromUserPassword,
-			String toUserEmailIDList, String additionalBodyContent)
+			String toUserEmailIDList, String additionalBodyContent, Logger log)
 			throws Exception {
+		log.debug("getting eml file location for emailQueueId:\""
+				+ emailQueueId + "\" at:\"" + EmailManager.getDate() + "\".");
 		String emlFilePath = OrderEmailQueueServices
 				.getEMLFileLocation(emailQueueId);
+		log.debug("Got eml file location:\"" + emlFilePath
+				+ "\" for emailQueueId:\"" + emailQueueId + "\" at:\""
+				+ EmailManager.getDate() + "\".");
+		log.debug("Forwarding eml file in Email for fromUserEmailID:\""
+				+ fromUserEmailID + "\" to toUserEmailIDList:\""
+				+ toUserEmailIDList + "\" at:\"" + EmailManager.getDate()
+				+ "\".");
 		forwardEML(fromUserEmailID, fromUserPassword, toUserEmailIDList,
-				emlFilePath, additionalBodyContent);
+				emlFilePath, additionalBodyContent, log);
 
 	}
 
